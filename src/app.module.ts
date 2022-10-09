0257233fd18c520efce03e10f5e99f157b87f9a1
Niklas Nihlén <niklas.nihlen@gmail.com>
2022-10-09 11:35:21 +0200

    chore: removing unused test stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsControllerV1 } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 import { HealthController } from './health/health.controller';
 import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsControllerV1, HealthController, DogsController],
-  providers: [AppService, CatsService],
+  controllers: [
+    AppController,
+    CatsControllerV1,
+    HealthController,
+    DogsController,
+  ],
+  providers: [AppService],
 })
 export class AppModule {}
