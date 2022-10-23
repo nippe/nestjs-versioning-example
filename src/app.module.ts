60aa1a41b8742020e2e44bde1195e8ace137f13b
Niklas Nihlén <niklas.nihlen@gmail.com>
2022-10-23 11:24:18 +0200

    chore: cleaning out unused controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsControllerV1 } from './cats/cats.controller';
 import { HealthController } from './health/health.controller';
-import { DogsController } from './dogs/dogs.controller';
 import { MultiVersionSameControllerController } from './multi-version-same-controller/multi-version-same-controller.controller';
 import { VersionOneController } from './version-one/version-one.controller';
 import { VersionTwoController } from './version-two/version-two.controller';
@@ -12,13 +10,11 @@ import { VersionTwoController } from './version-two/version-two.controller';
   imports: [],
   controllers: [
     AppController,
-    CatsControllerV1,
     HealthController,
-    DogsController,
     MultiVersionSameControllerController,
     VersionOneController,
     VersionTwoController,
   ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/dogs/dogs.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('dogs')
-export class DogsController {
-  @Get()
-  findAll(): string {
-    return 'Returning all the dogs using defaultVersion attribute';
-  }
-}
