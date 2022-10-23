731f04c61a5c2d20b86a38da3ce335e64e9fed7f
Niklas Nihlén <niklas.nihlen@gmail.com>
2022-10-23 14:54:37 +0200

    feat: exposes OpenApi defintion on the /api endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,15 @@ async function bootstrap() {
     defaultVersion: ['2', '1'],
   });
   // app.setGlobalPrefix('api');   // * Will add a prefixt to path before version /api/v1/.... /api/v2/...
+
+  // swagger
+  const specConfig = new DocumentBuilder()
+    .setTitle('Versioning Sample App')
+    .setDescription('A sample app showing off some HTTP versioning concepts')
+    .build();
+  const openApiDoc = SwaggerModule.createDocument(app, specConfig);
+  SwaggerModule.setup('api', app, openApiDoc);
+  // swagger end
   await app.listen(3030);
 }
 bootstrap();
